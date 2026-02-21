c08691399f5398f09fbe01e26e11baf6a7a9ef4c
Eigen-0x41 <70758356+Eigen-0x41@users.noreply.github.com>
2026-02-22 01:28:41 +0900

    AssemblyDescriptionの変更

--- InputSound/Properties/AssemblyInfo.cs
+++ InputSound/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // 制御されます。アセンブリに関連付けられている情報を変更するには、
 // これらの属性値を変更してください。
 [assembly: AssemblyTitle("A Dance of Fire and Ice: Input Sound Mod")]
-[assembly: AssemblyDescription("Plays HitSound.wav when a key is pressed.")]
+[assembly: AssemblyDescription("Plays a hit sound when a key is pressed.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("InputSound")]
