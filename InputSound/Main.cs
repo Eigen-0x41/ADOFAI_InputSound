4bf0882f47d630e2eb4b30a86e4239a2c0876c9e
Eigen-0x41 <70758356+Eigen-0x41@users.noreply.github.com>
2026-03-08 06:19:43 +0900

    試験的に全てのエフェクトをキー入力でのみ受け付けられるようにする。

--- InputSound/Main.cs
+++ InputSound/Main.cs
@@ -1,18 +1,29 @@
 ﻿using HarmonyLib;
 using SkyHook;
 using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
 using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Threading;
 using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.Rendering;
 using UnityModManagerNet;
 
 namespace InputSound
 {
     static class Main
     {
-        public static UnityModManager.ModEntry ModEntry;
+        public static UnityModManager.ModEntry.ModLogger Logger;
 
+        [Conditional("DEBUG")]
+        static public void DebugPrinting(string str)
+        {
+            Logger.Log(str);
+        }
 
         public static bool Load(UnityModManager.ModEntry modEntry)
         {
@@ -20,7 +31,9 @@ namespace InputSound
             harmony.PatchAll(Assembly.GetExecutingAssembly());
             scrConductorPatch.HitSoundsDataPatch.LoadMannual(modEntry, harmony);
 
-            ModEntry = modEntry;
+            Logger = modEntry.Logger;
+
+            DebugPrinting("Enable Debug Log Enable.");
 
             return true;
         }
@@ -31,13 +44,25 @@ namespace InputSound
     {
         private static void PlayHitSound()
         {
-            var scrCondIns = scrConductor.instance;
-            if (scrCondIns == null) return;
-            AudioManager.Play("snd" + scrCondIns.hitSound, 0, scrCondIns.hitSoundGroup, scrCondIns.hitSoundVolume, 10);
+            try
+            {
+                AudioManagerPatch.UpdateToLatest();
+
+                var audSrc = AudioManagerPatch.CurrentHitSound;
+                if (audSrc == null)
+                    return;
+
+                //audSrc.volume = scrConductor.instance.hitSoundVolume;
+                audSrc.Play();
+            }
+            catch (Exception e)
+            {
+                Main.Logger.LogException("PlayHitSound()", e);
+            }
         }
 
         [HarmonyPatch("HookCallback", new Type[] { typeof(SkyHookEvent) }), HarmonyPrefix]
-        public static void HookCallbackPrefix(SkyHookManager __instance, SkyHookEvent ev)
+        private static void HookCallbackPrefix(SkyHookManager __instance, SkyHookEvent ev)
         {
             if (ev.Type != SkyHook.EventType.KeyPressed)
                 return;
@@ -52,6 +77,53 @@ namespace InputSound
         }
     }
 
+    [HarmonyPatch(typeof(AudioManager))]
+    public class AudioManagerPatch
+    {
+        public static AudioSource CurrentHitSound => (currentDelay != double.MaxValue) ? hitSoundBuffer.First().Value : null;
+        private static double currentDelay => (hitSoundBuffer.Count > 0) ? hitSoundBuffer.First().Key : double.MaxValue;
+
+        private static SortedList<double, AudioSource> hitSoundBuffer = new SortedList<double, AudioSource>();
+
+        private static readonly DateTime GameDateTime = DateTime.Now;
+
+        [HarmonyPatch(nameof(AudioManager.Play), new Type[] { typeof(string), typeof(double), typeof(AudioMixerGroup), typeof(float), typeof(int) }), HarmonyPrefix]
+        private static bool HookCallbackPrefix(string snd, double time, AudioMixerGroup group, float volume, int priority)
+        {
+            var __instance = AudioManager.Instance;
+
+            AudioSource audioSource = __instance.MakeSource(snd, 0);
+            audioSource.pitch = 1f;
+            if (group != null)
+            {
+                audioSource.outputAudioMixerGroup = group;
+            }
+            else
+            {
+                audioSource.outputAudioMixerGroup = __instance.fallbackMixerGroup;
+            }
+
+            audioSource.volume = volume;
+            audioSource.priority = priority;
+            float num = (audioSource.clip ? audioSource.clip.length : float.PositiveInfinity);
+
+            if (!hitSoundBuffer.ContainsKey(time))
+                hitSoundBuffer.Add(time, audioSource);
+
+            return false;
+        }
+
+        public static void UpdateToLatest()
+        {
+            double dspTime = scrConductor.instance.dspTime - 0.03125;
+            while (currentDelay < dspTime)
+            {
+                Main.DebugPrinting($"HookCallback: [Operation: delete], {currentDelay}, {dspTime}");
+                hitSoundBuffer.Remove(currentDelay);
+            }
+        }
+    }
+
     [HarmonyPatch(typeof(scrConductor))]
     public class scrConductorPatch
     {
@@ -59,15 +131,15 @@ namespace InputSound
         {
             public static void LoadMannual(in UnityModManager.ModEntry modEntry, Harmony harmony)
             {
-                var Asembly = Assembly.GetAssembly(typeof(scrConductor));
-                var mOriginal = AccessTools.Constructor(Asembly.GetType($"{nameof(scrConductor)}+HitSoundsData"), new Type[] { typeof(HitSound), typeof(double), typeof(float) });
-                modEntry.Logger.Log($"Original: {mOriginal}");
-                var mPrefix = AccessTools.Method(typeof(HitSoundsDataPatch), "HitSoundsDataPrefix");
-                modEntry.Logger.Log($"Prefix  : {mPrefix}");
-                harmony.Patch(mOriginal, new HarmonyMethod(mPrefix));
+                // var Asembly = Assembly.GetAssembly(typeof(scrConductor));
+                // var mOriginal = AccessTools.Constructor(Asembly.GetType($"{nameof(scrConductor)}+HitSoundsData"), new Type[] { typeof(HitSound), typeof(double), typeof(float) });
+                // modEntry.Logger.Log($"Original: {mOriginal}");
+                // var mPrefix = AccessTools.Method(typeof(HitSoundsDataPatch), "HitSoundsDataPrefix");
+                // modEntry.Logger.Log($"Prefix  : {mPrefix}");
+                // harmony.Patch(mOriginal, new HarmonyMethod(mPrefix));
             }
 
-            public static void HitSoundsDataPrefix(HitSound hitSound, double time, ref float volume)
+            private static void HitSoundsDataPrefix(HitSound hitSound, double time, ref float volume)
             {
                 volume = 0.0f;
             }
