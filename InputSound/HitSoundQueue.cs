e5937a88bbe837929592716af92fe66bfe009cf5
Eigen-0x41 <70758356+Eigen-0x41@users.noreply.github.com>
2026-03-17 10:19:41 +0900

    判定処理の変更とそれに伴うリファクタ。

---
+++ InputSound/HitSoundQueue.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.Audio;
+
+namespace InputSound
+{
+    internal class HitSoundQueue
+    {
+        private SortedList<double, AudioSourceInfomation> hitSoundBuffer = new SortedList<double, AudioSourceInfomation>();
+        private SortedSet<double> keyReleaseDelay = new SortedSet<double>();
+
+        static PriorityBuffer[] adaptivePriority = new PriorityBuffer[4] { new PriorityBuffer(), new PriorityBuffer(), new PriorityBuffer(), new PriorityBuffer() };
+
+        private int GenAdditionalPriority(string snd, int priority)
+        {
+            const int MAX_BUFFER_SIZE = 4;
+            int matchCount = 0;
+            foreach (var value in adaptivePriority)
+            {
+                if (value.Snd != snd)
+                    break;
+                if (value.Priority != priority)
+                    break;
+                matchCount++;
+            }
+
+            if (matchCount == MAX_BUFFER_SIZE)
+                return 0;
+
+            adaptivePriority[matchCount].Init(snd, priority);
+            return MAX_BUFFER_SIZE - matchCount;
+        }
+
+        private bool IsDoingAddValue(double time, int priority, int additionalPriority)
+        {
+            AudioSourceInfomation audioSourceInfo = null;
+            if (hitSoundBuffer.TryGetValue(time, out audioSourceInfo))
+            {
+                if (priority != audioSourceInfo.AudioSource.priority)
+                    return false;
+
+                if (additionalPriority <= audioSourceInfo.AdditionalPriority)
+                    return false;
+            }
+            return true;
+        }
+
+        public void EnrollHitSound(string snd, double time, AudioMixerGroup group, float volume, int priority)
+        {
+            int additionalPriority = GenAdditionalPriority(snd, priority);
+            if (IsDoingAddValue(time, priority, additionalPriority))
+            {
+
+                var __instance = AudioManager.Instance;
+
+                AudioSource audioSource = __instance.MakeSource(snd, 0);
+                audioSource.pitch = 1f;
+
+                if (group != null)
+                    audioSource.outputAudioMixerGroup = group;
+                else
+                    audioSource.outputAudioMixerGroup = __instance.fallbackMixerGroup;
+
+                audioSource.volume = volume;
+                audioSource.priority = priority;
+                float num = (audioSource.clip ? audioSource.clip.length : float.PositiveInfinity);
+
+                hitSoundBuffer[time] = new AudioSourceInfomation(audioSource, additionalPriority);
+
+            }
+
+            UpdateLatest();
+        }
+        public void EnrollReleaseHitSound(double endTime)
+        {
+            keyReleaseDelay.Add(endTime);
+            UpdateLatest();
+        }
+
+        private long interLockRunningUpdate = 0;
+        private async void UpdateLatest()
+        {
+            const long NOT_RUNNING_UPDATE_LATEST = 0;
+            const long RUNNING_UPDATE_LATEST = 1;
+
+            if (Interlocked.Exchange(ref interLockRunningUpdate, RUNNING_UPDATE_LATEST) == RUNNING_UPDATE_LATEST)
+                return;
+
+            double dspTime = scrConductor.instance.dspTime - 1.0;
+            await Task.Run(() =>
+                {
+                    while (hitSoundBuffer.Count > 0)
+                    {
+                        double targetDelay = hitSoundBuffer.Keys.First();
+                        if (dspTime > targetDelay)
+                        {
+                            Main.DebugPrinting($"TryUpdateLatest: [Operation: delete], {targetDelay}, {dspTime}");
+                            hitSoundBuffer.Remove(targetDelay);
+                            continue;
+                        }
+                        break;
+                    }
+
+                    while (keyReleaseDelay.Count > 0)
+                    {
+                        double targetDelay = keyReleaseDelay.First();
+                        if (dspTime > targetDelay)
+                        {
+                            Main.DebugPrinting($"TryUpdateLatest[KeyRelease]: [Operation: delete], {targetDelay}, {dspTime}");
+                            keyReleaseDelay.Remove(targetDelay);
+                            continue;
+                        }
+                        break;
+                    }
+
+                    Interlocked.Exchange(ref interLockRunningUpdate, NOT_RUNNING_UPDATE_LATEST);
+                });
+        }
+
+        private bool IsDoingReleaseHitSound(double late, double early)
+        {
+            double average = (late + early) / 2;
+            return keyReleaseDelay.Any((a) =>
+            {
+                var locAverage = (a + average) / 2;
+                return (late < locAverage) && (locAverage < early);
+            });
+        }
+
+        public bool TryGetHitSound(double dspTime, bool isKeyPressed, out AudioSource audioSource)
+        {
+            double currentHitSoundDelay = dspTime;
+
+            var lateHitSoundPair = hitSoundBuffer.LastOrDefault((a) => a.Key < dspTime);
+            var earlyHitSoundPair = hitSoundBuffer.FirstOrDefault((a) => dspTime < a.Key);
+            if (!hitSoundBuffer.TryGetValue(dspTime, out AudioSourceInfomation audioSourceInfo))
+            {
+                double average = (lateHitSoundPair.Key + earlyHitSoundPair.Key) / 2.0;
+                if (dspTime < average)
+                {
+                    audioSourceInfo = lateHitSoundPair.Value;
+                }
+                else
+                {
+                    audioSourceInfo = earlyHitSoundPair.Value;
+                }
+
+            }
+
+            audioSource = null;
+
+            if (audioSourceInfo != null)
+                if (IsDoingReleaseHitSound(lateHitSoundPair.Key, earlyHitSoundPair.Key) || isKeyPressed)
+                    audioSource = audioSourceInfo.AudioSource;
+
+            return audioSource != null;
+        }
+
+        private class PriorityBuffer
+        {
+            public string Snd = string.Empty;
+            public int Priority = 0;
+
+            public void Init(string soundName, int priority)
+            {
+                Snd = soundName;
+                Priority = priority;
+            }
+        }
+        private class AudioSourceInfomation
+        {
+            public AudioSource AudioSource = null;
+            public int AdditionalPriority = 0;
+
+            public AudioSourceInfomation(AudioSource audioSource, int additionalPriority = 0)
+            {
+                AudioSource = audioSource;
+                AdditionalPriority = additionalPriority;
+            }
+        }
+    }
+}
--- InputSound/Main.cs
+++ InputSound/Main.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Reflection;
+using System.Threading;
 using System.Threading.Tasks;
 using UnityEngine;
 using UnityEngine.Audio;
@@ -38,24 +39,14 @@ namespace InputSound
     [HarmonyPatch(typeof(SkyHookManager))]
     internal class SkyHookManagerPatch
     {
-        private static void PlayHitSound(bool isKeyReleased)
+        public static HitSoundQueue hitSoundQueue = new HitSoundQueue();
+        private static void PlayHitSound(double dspTime, bool isKeyPressed)
         {
-            try
-            {
-                if (isKeyReleased && !AudioManagerPatch.UpdateAndDoReleaseHitSound())
-                    return;
+            AudioSource audSrc = null;
+            if (!hitSoundQueue.TryGetHitSound(dspTime, isKeyPressed, out audSrc))
+                return;
 
-                var audSrc = AudioManagerPatch.CurrentHitSound;
-                if (audSrc == null)
-                    return;
-
-                //audSrc.volume = scrConductor.instance.hitSoundVolume;
-                audSrc.Play();
-            }
-            catch (Exception e)
-            {
-                Main.Logger.LogException("PlayHitSound()", e);
-            }
+            audSrc.Play();
         }
 
         [HarmonyPatch("HookCallback", new Type[] { typeof(SkyHookEvent) }), HarmonyPrefix]
@@ -67,83 +58,34 @@ namespace InputSound
             if (srcControllerInstance.paused)
                 return;
 
-            Task.Run(() => PlayHitSound(ev.Type == SkyHook.EventType.KeyReleased));
+            _ = Task.Run(() => PlayHitSound(scrConductor.instance.dspTime, ev.Type == SkyHook.EventType.KeyPressed));
         }
     }
 
     [HarmonyPatch(typeof(AudioManager))]
     internal class AudioManagerPatch
     {
-        public static AudioSource CurrentHitSound => (hitSoundBuffer.Count > 0) ? hitSoundBuffer.Values[0] : null;
-        private static double currentDelay => (hitSoundBuffer.Count > 0) ? hitSoundBuffer.Keys[0] : double.MaxValue;
-        private static double previousDelay = double.MaxValue;
-
-        private static SortedList<double, AudioSource> hitSoundBuffer = new SortedList<double, AudioSource>();
-
-        private static readonly DateTime GameDateTime = DateTime.Now;
-
         [HarmonyPatch(nameof(AudioManager.Play), new Type[] { typeof(string), typeof(double), typeof(AudioMixerGroup), typeof(float), typeof(int) }), HarmonyPrefix]
-        private static bool HookCallbackPrefix(string snd, double time, AudioMixerGroup group, float volume, int priority)
+        private static bool PlayPrefix(string snd, double time, AudioMixerGroup group, float volume, int priority)
         {
             // ノーツ音ではないヒットサウンドの検出用。
             // ADOFAI本体にハードコードされています。
             if (priority == 10)
                 return true;
 
-            var __instance = AudioManager.Instance;
-
-            AudioSource audioSource = __instance.MakeSource(snd, 0);
-            audioSource.pitch = 1f;
-
-            if (group != null)
-                audioSource.outputAudioMixerGroup = group;
-            else
-                audioSource.outputAudioMixerGroup = __instance.fallbackMixerGroup;
-
-            audioSource.volume = volume;
-            audioSource.priority = priority;
-            float num = (audioSource.clip ? audioSource.clip.length : float.PositiveInfinity);
-
-            if (!hitSoundBuffer.ContainsKey(time))
-                hitSoundBuffer.Add(time, audioSource);
+            SkyHookManagerPatch.hitSoundQueue.EnrollHitSound(snd, time, group, volume, priority);
 
             return false;
-        }
-
-        public static bool UpdateAndDoReleaseHitSound()
-        {
-            var scrCondIns = scrConductor.instance;
-            double dspTime = scrCondIns.dspTime;
-            while (dspTime > currentDelay)
-            {
-                Main.DebugPrinting($"HookCallback: [Operation: delete], {currentDelay}, {dspTime}");
-                previousDelay = currentDelay;
-                hitSoundBuffer.Remove(currentDelay);
-            }
-
-            return scrConductorPatch.DoReleaseHitSound(dspTime, previousDelay, currentDelay);
         }
     }
 
     [HarmonyPatch(typeof(scrConductor))]
     internal class scrConductorPatch
     {
-        private static double currentDelay => (keyReleaseSoundSet.Count > 0) ? keyReleaseSoundSet.First() : double.MaxValue;
-
-        private static SortedSet<double> keyReleaseSoundSet = new SortedSet<double>();
-
-        public static bool DoReleaseHitSound(double dspTime, double previousDelay, double currentPlaingDelay)
-        {
-            while (dspTime > currentDelay)
-                keyReleaseSoundSet.Remove(currentDelay);
-
-            return (previousDelay < currentDelay) && (currentDelay < currentPlaingDelay);
-        }
-
         [HarmonyPatch(nameof(scrConductor.PlayWithEndTime), new Type[] { typeof(string), typeof(double), typeof(double), typeof(float), typeof(int) }), HarmonyPrefix]
         private static void PlayWithEndTimePrefix(string snd, double time, double endTime, float volume = 1f, int priority = 128)
         {
-            keyReleaseSoundSet.Add(endTime);
+            SkyHookManagerPatch.hitSoundQueue.EnrollReleaseHitSound(endTime);
         }
     }
 }
